f88f362cec99e99cff2338ee4bb94edd1bba3f30
Godswill Aniakor <godswill.aniakor@gmail.com>
2019-02-06 15:27:27 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome <code>src/App.js</code> Godzee!.
+            Welcome <code>src/App.js</code> Chiwuzie!.
           </p>
           <a
             className="App-link"
